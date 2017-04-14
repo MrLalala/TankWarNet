dd77a705518b81d258513477873c3a08bfc8ade9
Gan <1354410847@qq.com>
2017-04-14 19:47:43 +0800

    Edition N1.93：完善了部分代码，在连接上使用握手机制

--- NetClient.java
+++ NetClient.java
@@ -1,12 +1,27 @@
 package com.Tanknet.Gan;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+
+
 public class NetClient {
+	
+	private static int  UDP_PORT_START = 2333;
+	
+	private int udpPort;
+	
+	public NetClient(){
+		udpPort = UDP_PORT_START++;
+	}
+	
 	public void connect(String IP,int port){
 		try {
 			Socket s = new Socket(IP,port);
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			dos.writeInt(udpPort);
+			s.close();
 System.out.println("Server: "+ s.getLocalPort());
 		} catch (IOException e) {
 			e.printStackTrace();
--- TankServer.java
+++ TankServer.java
@@ -1,19 +1,31 @@
 package com.Tanknet.Gan;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TankServer {
 
 	public static final int TCP_PORT = 8888;
+	
 	private static ServerSocket ss;
-	public static void main(String[] args) {
+	
+	List<Client> clients = new ArrayList<Client>();
+	
+	public void start(){
 		try {
 			ss = new ServerSocket(TCP_PORT);
 			while(true){
 				Socket s = ss.accept();
 				System.out.println("A tankClient is Connect: "+s.getInetAddress()+"\t:"+s.getPort());
+				String ip = s.getLocalAddress().getHostAddress();
+				DataInputStream dis = new DataInputStream(s.getInputStream());
+				int port = dis.readInt();
+				clients.add(new Client(ip,port));
+				
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -23,6 +35,21 @@ public class TankServer {
 				e1.printStackTrace();
 			}
 		}
+	}
+	
+	public static void main(String[] args) {
+		new TankServer().start();
+	}
+	
+	private class Client{
+		private String IP;
+		private int port;
+		public Client(String IP, int port) {
+			this.IP = IP;
+			this.port = port;
+		}
+		
+		
 	}
 
 }
